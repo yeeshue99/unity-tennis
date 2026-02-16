7afd8e2d7dbf02249b8b1dd25cf980edfc7caf54
Michael Ngo <mingo@loc.gov>
2026-02-15 19:31:15 -0800

    update netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
